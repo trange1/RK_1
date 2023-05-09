a3c67f4a545d5819a1f31ef2c04ee5561fcfcfb1
trange1 <alex2308super@gmail.com>
2023-05-09 12:50:49 +0300

    17 to 23
    23

--- main.cpp
+++ main.cpp
@@ -1,16 +1,7 @@
-//
-// Created by alex2 on 18.03.2023.
-//
+#include <iostream>
 #include "tasks_rk1.h"
 
-using namespace std;
 int main() {
-   task_1();
-   task_2();
-   task_3();
-   task_4();
-   task_5();
-   task_6und7();
-
+    task_4();
+    return 0;
 }
-
